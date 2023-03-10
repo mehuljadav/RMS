48b2665ba850eaf18b5af44db25c17b49805a7a0
Mehul Jadav <mehuljadav.1989@gmail.com>
2023-03-10 17:40:16 +0530

    app.js file updated successfully

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
+
+//
+//
+// API Router Import
+//
+const PropertyRouter = require('./routes/propertyRoutes');
+const userRouter = require('./routes/userRouter');
+
+//
+//
+//Middleware
+//
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//
+//
+// Routs Middleware
+//
+app.use('/api/v1/properties', PropertyRouter);
+app.use('/api/v1/users', userRouter);
+
+//
+//
+// Handle unhandled Routes After all defined Routes
+//
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on Server!`, 500));
+});
+
+//
+//
+// Global Error Handler (err,req,res,next)
+//
+app.use(globalErrorHandler);
+
+//
+module.exports = app;
